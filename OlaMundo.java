69ee4b419626eee62fa2e5434375cee767881c58
juanpabss <juanpablodev2@gmail.com>
2024-06-26 20:21:09 -0300

    Implementação do Olar mundo robusto

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class OlaMundo{
     public static void main(String[] args){
-        System.out.println("Ola Mundo");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nOla, qual é o seu nome ?");
+        String nome = scanner.nextLine();
+        
+        bemVindo(nome);
+        // System.out.println("Ola Mundo");
+
+    }
+
+
+
+    public static void bemVindo(String nome){
+        System.out.println("Seja bem vindo(a) " + nome);
+        System.out.println(":)");
     }
 }
